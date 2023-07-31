1fa33d83a300caa8ac1143f86ddb889d2ae30aac
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-07-31 23:51:03 +0300

    🚨 Fix console log

--- resources/js/Language/index.js
+++ resources/js/Language/index.js
@@ -1,8 +1,6 @@
 import { createI18n } from 'vue-i18n'
 import {translates} from "@/Language/translates.js";
 
-console.log(translates)
-
 const i18n = createI18n({
     legacy: false,
     locale: 'tr',
